865b5db822fa89e99b1d24d3b8f6f76a06eff4fb
allangratts <allangrattsdossantos@outlook.com>
2021-08-10 14:15:17 -0300

    Preenchendo as funções com seus respectivos códigos

--- katas1.js
+++ katas1.js
@@ -1,94 +1,141 @@
 function oneThroughTwenty() {
+  
+  let nfinal = []
+  for (let n = 1; n <= 20; n++){
+    console.log(n);
+    nfinal.push (n);
     
-   /* Your code goes below
-   Write a for or a while loop
-   return the result*/
-    
+  }
+
+  return nfinal;
+
 }
 
-//call function oneThroughTwenty
+console.log (oneThroughTwenty ())
 
 function evensToTwenty() {
+  let nfinal = []
+  for (let n = 2; n <= 20; n += 2){
+    console.log(n);
+    nfinal.push (n);
     
-   /* Your code goes below
-   Write a for or a while loop
-   return the result */
+  }
+
+  return nfinal;
+}
+
+console.log (evensToTwenty ())
+
+function oddsToTwenty() {
+  let nfinal = []
+  for (let n = 1; n <= 20; n += 2){
+    console.log(n);
+    nfinal.push (n);
+    
+  }
+
+  return nfinal;
+}
+
+console.log (oddsToTwenty ())
+
+function multiplesOfFive() {
+  let nfinal = []
+  for (let n = 5; n <= 100; n ++){
+    if (n % 5 == 0) {
+      console.log(n);
+      nfinal.push (n);
+    
+    }
+    
+  }
+
+  return nfinal;
   
 }
 
-//call function evensToTwenty
-
-function oddsToTwenty() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-   
-}
-
-//call function oddsToTwenty
-
-function multiplesOfFive() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-
-}
-
-//call function multiplesOfFive
+console.log (multiplesOfFive())
 
 function squareNumbers() {
+  let nfinal = []
+  for (let n = 1; n <= 10; n ++){
+    console.log(n * n);
+    nfinal.push (n * n);
     
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  }
+
+  return nfinal;
     
 }
 
-//call function squareNumbers
+console.log (squareNumbers())
 
 function countingBackwards() {
+  let nfinal = []
+  for (let n = 20; n >= 1; n--){
+    console.log(n);
+    nfinal.push (n);
     
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  }
+
+  return nfinal;
 }
 
-//call function countingBackwards
+console.log (countingBackwards())
 
 function evenNumbersBackwards() {
+  let nfinal = []
+  for (let n = 20; n >= 2; n -= 2){
+    console.log(n);
+    nfinal.push (n);
     
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  }
+
+  return nfinal;
 }
 
-//call function evenNumbersBackwards
+console.log (evenNumbersBackwards())
 
 function oddNumbersBackwards() {
+  let nfinal = []
+  for (let n = 19; n >= 1; n -= 2){
+    console.log(n);
+    nfinal.push (n);
     
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  }
+
+  return nfinal;
 }
 
-//call function oddNumbersBackwards
+console.log (oddNumbersBackwards())
 
 function multiplesOfFiveBackwards() {
+  let nfinal = []
+  for (let n = 100; n >= 5; n--){
+    if (n % 5 == 0) {
+      console.log(n);
+      nfinal.push (n);
     
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+    }
+    
+  }
+
+  return nfinal;
 }
 
-//call function multiplesOfFiveBackwards
+console.log (multiplesOfFiveBackwards())
+
 
 function squareNumbersBackwards() {
+  let nfinal = []
+  for (let n = 10; n >= 1; n --){
     
-   /* Your code goes below
-  Write a for or a while loop
-  return the result */
+    console.log(n * n);
+    nfinal.push (n * n);
+  }
+
+  return nfinal;
 }
 
-//call function squareNumbersBackwards
+console.log (squareNumbersBackwards())
+
